2185ef10556148c6d465eafd18817dc20fa41e74
keshavnayyar <knayyar@mobiquityinc.com>
2022-08-11 10:13:07 +0530

    fmt

--- Project-Pink-Carrot/provider.tf
+++ Project-Pink-Carrot/provider.tf
@@ -1,21 +1,21 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.23.0"
     }
   }
 
   backend "s3" {
-    bucket = "thepinkcarrot"
-    key    = "xyz.terraform.tfstate"
-    region = "eu-west-1"
+    bucket  = "thepinkcarrot"
+    key     = "xyz.terraform.tfstate"
+    region  = "eu-west-1"
     profile = "interview"
   }
 }
 
 provider "aws" {
-  region = "eu-west-1" // Ireland as Default region
+  region                   = "eu-west-1" // Ireland as Default region
   shared_config_files      = ["/Users/knayyar/.aws/config"]
   shared_credentials_files = ["/Users/knayyar/.aws/credentials"]
   profile                  = "interview"
